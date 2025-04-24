a7a0e19b6a8404327ceb0d913a893b41e9899c5b
Muhidin3 <muhidinshemsu3@gmail.com>
2025-04-24 09:42:00 +0300

    .

---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+// app/error.tsx
+"use client";
+
+export default function Error({ error }: { error: Error }) {
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      <p>{error.message}</p>
+    </div>
+  );
+}
